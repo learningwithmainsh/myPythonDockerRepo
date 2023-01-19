8844a3152fe4ddf55ffe50893f7ddd5f16640d89
Manish Pandey <57837808+learningwithmainsh@users.noreply.github.com>
2023-01-19 19:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ images = [
 @app.route('/')
 def index():
     url = random.choice(images)
+    print("PRASAD KANSARE")
     return render_template('index.html', url=url)
 
 if __name__ == "__main__":
